5661a78709fdcc1cff9ce9149b1d64865f6999fd
sensationalkunlex <sensationalkunlex@gmail.com>
2018-04-18 22:32:53 +0100

    Add project files.

---
+++ ImageGetter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImageGetter.Startup))]
+namespace ImageGetter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
